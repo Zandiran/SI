e265826fedc44d54fff6c23d0060ec3725b831d9
PaulVP <vovchinap@gmail.com>
2022-03-14 15:22:39 +0300

    Golub

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -11,24 +11,24 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
+        long expected = 5;
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
-}
 
 
-  /*  @Test
+
+   @Test
     void findMax2() {
-        StatisticsService service = new StatisticsService();
+       StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 5;
+       long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+       long expected = 12;
 
-        long actual = service.findMax(incomesInBillions);
+       long actual = service.findMax(incomesInBillions);
 
-        assertEquals(expected, actual);
+       assertEquals(expected, actual);
 
-   */
+   }
+}
